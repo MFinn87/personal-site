dd45d410b4e55c5bfcf13749c10cfc031a9e2de4
Mike Finnegan <mfinnegan@Mikes-MacBook-Pro.local>
2020-10-25 02:35:06 -0600

    This is not working at all

--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  mounted() {
+    document.dispatchEvent(new Event('app.rendered'));
+  }
 })
 .$mount('#app');
